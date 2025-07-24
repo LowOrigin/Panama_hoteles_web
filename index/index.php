1a0d01da134e9f98d9f180f14fac889199b3a692
Erick <erick.ospina@utp.ac.pa>
2025-07-24 12:35:02 -0500

    Y ahora si index

---
+++ index/index.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+class IndexController
+{
+    public function handleRequest()
+    {
+        // Mensaje de bienvenida
+        if (isset($_SESSION['usuario'])) {
+            echo "<h2>Bienvenido, " . htmlspecialchars($_SESSION['usuario']) . "</h2>";
+        } else {
+            echo "<h2>Bienvenido a nuestro sitio de hoteles</h2>";
+        }
+
+        // Menú principal con enlaces relativos saliendo de /index hacia /public
+        echo "<ul>";
+        echo "<li><a href='../public/ver_hotel.php'>Ver Hoteles</a></li>";
+        echo "<li><a href='../public/reservar.php'>Reservar</a></li>";
+        echo "<li><a href='../public/login.php'>Iniciar Sesión</a></li>";
+        echo "<li><a href='../public/registro.php'>Registrarse</a></li>";
+        echo "</ul>";
+    }
+}
+
+// Ejecutar controlador
+$index = new IndexController();
+$index->handleRequest();
